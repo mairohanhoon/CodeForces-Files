6e74e379f43b6418b6755979f824643dad6d02a8
mairohanhoon <rohantiwari2709@gmail.com>
2023-07-01 12:44:48 +0530

    new questions

---
+++ george_and_accomodation.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+    int p[101];
+    int q[101];
+    int count = 0;
+    for(int i=0; i<n; i++){
+        cin >> p[i] >> q[i];
+        if(q[i]-p[i] >= 2){
+            count++;
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
