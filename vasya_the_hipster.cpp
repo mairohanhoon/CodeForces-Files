54cfeca52c315eb4b9a8681bfb9044129066e548
mairohanhoon <rohantiwari2709@gmail.com>
2023-07-17 16:50:02 +0530

    new questions solved

---
+++ vasya_the_hipster.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+
+    int a, b;
+    cin >> a >> b;
+    cout << min(a, b) << " " << (max(a,b)-min(a,b))/2 << endl;
+
+    return 0;
+}
