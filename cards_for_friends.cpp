ecc4266d1afdf28f578e43d9106015f29700f469
mairohanhoon <rohantiwari2709@gmail.com>
2023-07-31 11:07:06 +0530

    new questions solved

---
+++ cards_for_friends.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin >> t;
+    while(t--){
+        int x, y, n;
+        cin >> x >> y >> n;
+        int count = 0;
+        while(x>1 && x%2==0){
+            count++;
+            x /= 2;
+        }
+        while(y>1 && y%2==0){
+            count++;
+            y /= 2;
+        }
+        count = pow(2, count);
+        if(count >= n){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
