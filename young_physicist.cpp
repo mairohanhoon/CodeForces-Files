132b188adc68ba2547232248da42f0e7c2c43f65
mairohanhoon <rohantiwari2709@gmail.com>
2023-08-09 22:04:38 +0530

    new questions solved

--- young_physicist.cpp
+++ young_physicist.cpp
@@ -6,19 +6,21 @@ int main(){
     int n;
     cin >> n;
     int sum = 0;
-    while(n--){
-        int A[3];
-        for(int i=0; i<3; i++){
-            cin >> A[i];
-            sum = sum + A[i];
-        }
+    int arr[n][3];
+    int sum1=0;
+    int sum2 = 0;
+    int sum3 = 0;
+    for(int i=0; i<n; i++){
+        cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
+        sum1+=arr[i][0];
+        sum2+=arr[i][1];
+        sum3+=arr[i][2];
     }
-    if(sum==0){
+    if(sum1==0 && sum2==0 && sum3==0){
         cout << "YES" << endl;
     }
     else{
         cout << "NO" << endl;
     }
-
     return 0;
 }
