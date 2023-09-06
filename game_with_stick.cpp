d8c9c750a59d99a5c5e674a7027fe8207b68b8fe
mairohanhoon <rohantiwari2709@gmail.com>
2023-09-06 18:27:18 +0530

    new questions solved

---
+++ game_with_stick.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n, m;
+    cin >> n >> m;
+    int check;
+    if(n == 1 || m == 1){
+        check = 1;
+    }
+    else{
+        check = n*m;
+    }
+
+    if(check % 2 == 0){
+        cout << "Malvika" << endl;
+    }
+    else{
+        cout << "Akshat" << endl;
+    }
+
+    return 0;
+}
