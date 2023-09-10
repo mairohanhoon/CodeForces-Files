47e7e55bd1f8b1a1b1108fe13d8c136b7762903f
mairohanhoon <rohantiwari2709@gmail.com>
2023-09-10 10:23:27 +0530

    new questions solved

---
+++ divisibility_problem.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin >> t;
+    while(t--){
+        int a, b;
+        cin >> a >> b;
+        if(a%b == 0){
+            cout << 0 << endl;
+        }
+        else{
+            cout << b - (a%b) << endl;
+        }
+    }
+
+    return 0;
+}
