8bf3ef2abb2dcbc8f51e574d4de3fa6743e67c7f
mairohanhoon <rohantiwari2709@gmail.com>
2023-11-03 18:34:57 +0530

    addednew problem

---
+++ twins.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[n];
+    }
+    sort(arr, arr+n);
+    if(n%2 != 0){
+        cout << (n/2)+1 << endl;
+    }
+    else{
+        if(arr[0] != arr[n-1]){
+            cout << (n/2)<< endl;
+        }
+        else{
+            cout << (n/2)+1 << endl;
+        }
+    }
+
+    return 0;
+}
