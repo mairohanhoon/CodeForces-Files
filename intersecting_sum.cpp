d771c3db177fc4045ed540f98d4937ebdf17e8e5
mairohanhoon <rohantiwari2709@gmail.com>
2023-12-28 14:47:24 +0530

    Added new problems

---
+++ intersecting_sum.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int arr[n];
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+        }
+        sort(arr, arr+n);
+        int res = arr[n-1] - arr[0];
+        res+= arr[n-2]-arr[1];
+        cout << res << endl;
+    }
+
+    return 0;
+}
