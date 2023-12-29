de39894134f478f428489c6a94cb69dd8c104c71
mairohanhoon <rohantiwari2709@gmail.com>
2023-12-29 15:49:36 +0530

    Added new problems

--- unnatural_language_processing.cpp
+++ unnatural_language_processing.cpp
@@ -1,44 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-
+bool isVowel (char ch) {
+    return ch == 'a' || ch == 'e';
+}
+ 
+int main() {
+    
     int t;
     cin >> t;
     while(t--){
         int n;
-        cin >> n;
         string str;
-        cin >> str;
-        int i=n-1;
-        string ans;
-        while(i>=0){
-            string temp = "";
-            if(str[i] != 'a' && str[i] != 'e'){
-                temp += str[i-2];
-                temp+= str[i-1];
-                temp += str[i];
-                i-=3;
-                ans = temp+ans;
-                if(i>=0){
-                    string check = ".";
-                    ans = check +ans;
-                }
-            }
-            else{
-                temp+= str[i-1];
-                temp += str[i];
-                i-=2;
-                ans = temp+ans;
-                if(i>=0){
-                    string check = ".";
-                    ans = check +ans;
-                }
+        cin >> n >> str;
+        
+        string ans = "";
+        
+        for (int i = n - 1; i >= 0; i--) {
+            if (isVowel(str[i]) && !isVowel(str[i - 1])) {
+                ans += str[i];
+                ans += str[i - 1];
+                ans += '.';
+                i--;
+            } else {
+                ans += str[i];
             }
         }
-        string res = "";
+        
+        ans.pop_back();
+        
+        reverse(ans.begin(), ans.end());
         cout << ans << endl;
     }
-
-    return 0;
+    
 }
